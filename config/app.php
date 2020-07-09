a444ab3457badbcfd70441cb176b2996736cd715
Sibongiseni Msomi <siboingiseni.msomi@outlook.com>
2020-07-09 15:35:45 +0200

    no message

--- config/app.php
+++ config/app.php
@@ -126,9 +126,9 @@ return [
     |
     */
 
-    'smsportal_client_id' => env('SMSPORTAL_CLIENT_ID', ''),
-    'smsportal_api_secret' => env('SMSPORTAL_API_SECRET', ''),
-    'smsportal_test_mode' => env('SMSPORTAL_TEST_MODE', 1),
+    'smsportal_client_id' => env('SMSPORTAL_CLIENT_ID'),
+    'smsportal_api_secret' => env('SMSPORTAL_API_SECRET'),
+    'smsportal_test_mode' => env('SMSPORTAL_TEST_MODE'),
 
     /*
     |--------------------------------------------------------------------------
