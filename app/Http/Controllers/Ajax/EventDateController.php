38e4a5f6784b38997907f442f0b32c0c88643c1f
Sibongiseni Msomi <siboingiseni.msomi@outlook.com>
2020-07-11 20:13:50 +0200

    Prevent Full Events Date from showing

--- app/Http/Controllers/Ajax/EventDateController.php
+++ app/Http/Controllers/Ajax/EventDateController.php
@@ -16,7 +16,7 @@ class EventDateController extends Controller
         $user = Auth::user();
 
         if($user->hasAnyRole([User::SUPER_ADMIN_ROLE, User::ADMIN_ROLE])){
-            $event_dates = EventDate::whereDate('date_time', '>=', Carbon::now())->orderBy('date_time')->get();
+            $event_dates = EventDate::whereDate('date_time', '>=', Carbon::now())->where(['status_id', EventDate::STATUS_ACTIVE])->orderBy('date_time')->get();
         }elseif($user->hasRole(USER::CLIENT_ROLE)){
             $appointments = [];
 
